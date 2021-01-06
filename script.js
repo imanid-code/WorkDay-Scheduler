d4a566abb6b70e62481e44354083f6cdf98422a4
Imani R Dillahunt <imanirdillahunt@Imanis-MacBook-Air.local>
2021-01-06 11:24:57 -0500

    local storage and consolelog

--- script.js
+++ script.js
@@ -11,16 +11,49 @@ $(document).ready(function(){
     $(".Hour").each(function(i){
         $(this).text(moment().hour(i+9).format("h A"))
        //? Where is the 9 coming from .. is it because of the length of the array?
+       // moment functioning 
+       //.each a way to iterate over jquerry elements.every hour class set text to a time 9 as i increases by 1
     })
     console.log(currentTime);
-    
+
+    // var test = {...localStorage}
+    // console.log(test)
+    //write over and over for each time block. Change id as well 
+    var localNine = localStorage.getItem('9am-todo');
+    $("#0").val(localNine)
+    console.log(localNine)
+    var localTen = localStorage.getItem('10am-todo');
+    $("#1").val(localTen)
+    console.log(localTen)
+    var localElleven = localStorage.getItem('11am-todo');
+    $("#2").val(localElleven);
+    console.log(localElleven);
+    var localTwelve = localStorage.getItem('12pm-todo');
+    $("#3").val(localTwelve);
+    console.log(localTwelve);
+    var localOne = localStorage.getItem('1pm-todo');
+    $("#4").val(localOne);
+    console.log(localOne);
+    var localTwo = localStorage.getItem('2pm-todo');
+    $("#5").val(localTwo);
+    console.log(localTwo);
+    var localThree = localStorage.getItem('3pm-todo');
+    $("#6").val(localThree);
+    console.log(localThree);
+    var localFour = localStorage.getItem('4pm-todo');
+    $("#7").val(localFour);
+    console.log(localFour);
+    var localFive = localStorage.getItem('5pm-todo');
+    $("#8").val(localFive);
+    console.log(localFive);
     
     //HourRow is the row where the hour is displayed. Where you add your 2-do
     //.each?
     //indexOf currentTime - goes through each time of the array
-    //$(`#${i}`)
-    //  $(`#saveToDo-${i}`).prop('disabled', true);
-   // $(`#saveToDo-${i}`).addClass('past');
+    //$(`#${i}`) - templete  literal , way to write java script inside of a string - back tics is the same a quote but allows for templete literal
+    //  $(`#saveToDo-${i}`).prop('disabled', true); - i iterates whichever iteration we are on . prop manipulates a property inside element(id class value style)
+   // add prop disabled and set to true 
+    // $(`#saveToDo-${i}`).addClass('past');
     //$(`#hour-${i}`).addClass('past');
     // $('input, select, textarea').each(function() {
         //var value = $(this).val(),
@@ -28,7 +61,6 @@ $(document).ready(function(){
 
 
     $('.HourRow').each(function(i) {
-        console.log(timeArray.indexOf(currentTime));
         if (timeArray.indexOf(currentTime) > i) {
           $(`#${i}`).addClass('past');
           $(`#saveToDo-${i}`).prop('disabled', true);
@@ -55,5 +87,6 @@ $(document).ready(function(){
            var value = $(this).val(),
                name = $(this).attr('name');
            localStorage[name] = value;
+
                    
       })});
